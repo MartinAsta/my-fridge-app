27d82ddb93515861f68ce38ad46004ad073aae2a
Student <martin.asta@hotmail.com>
2026-06-09 15:34:45 +0200

    small change

--- backend/app/models/user_model.py
+++ backend/app/models/user_model.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, String, func
+from sqlalchemy import DateTime, Identity, String, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from ..database import Base
@@ -9,7 +9,7 @@ from ..database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(Identity(always=True),primary_key=True, index=True)
     email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
     username: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
